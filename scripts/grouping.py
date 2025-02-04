78266dd56e5777e96c672cf6834fa7cbd66319c1
niconunezz <niconuwii@gmail.com>
2025-02-04 20:54:58 +0100

    winning against torch

--- scripts/grouping.py
+++ scripts/grouping.py
@@ -3,120 +3,126 @@ import torch
 import triton.language as tl
 import random
 
+
+
+@triton.autotune(
+    configs=[
+
+        triton.Config({"Br": 128, "Bc": 256, "Bk": 32, "GROUP_SZE_M": 4}, num_stages=3, num_warps=8),
+        triton.Config({"Br": 64, "Bc": 32, "Bk": 64, "GROUP_SZE_M": 8}, num_stages=4, num_warps=8),
+        triton.Config({"Br": 128, "Bc": 128, "Bk": 32, "GROUP_SZE_M": 4}, num_stages=5, num_warps=8),
+        triton.Config({"Br": 128, "Bc": 128, "Bk": 64, "GROUP_SZE_M": 2}, num_stages=4, num_warps=8),
+  
+    ],
+    key=["M", "N", "K"],
+)
+
 @triton.jit
-def grouping(A, B, C, alpha, beta,
+def grouping(A, B, C,
+                am_stride, ak_stride,
+                bk_stride, bn_stride,
+                cm_stride, cn_stride,
+                M, N, K,
                 Br:tl.constexpr, Bc:tl.constexpr, Bk:tl.constexpr,
                 GROUP_SZE_M:tl.constexpr,
-                am_stride:tl.constexpr, ak_stride:tl.constexpr,
-                bk_stride:tl.constexpr, bn_stride:tl.constexpr,
-                cm_stride:tl.constexpr, cn_stride:tl.constexpr,
-                M:tl.constexpr, N:tl.constexpr, K:tl.constexpr
                 ):
+    # import pdb; pdb.set_trace()
+    pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, Br)
+    num_pid_n = tl.cdiv(N, Bc)
+    num_pid_in_group = GROUP_SZE_M * num_pid_n
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_SZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SZE_M)
+    pid_m = first_pid_m + ((pid % num_pid_in_group) % group_size_m)
+    pid_n = (pid % num_pid_in_group) // group_size_m
+
+    offs_am = (pid_m * Br + tl.arange(0, Br)) % M
+    offs_bn = (pid_n * Bc + tl.arange(0, Bc)) % N
+
+    offs_am = tl.max_contiguous(tl.multiple_of(offs_am, Br), Br)
+    offs_bn = tl.max_contiguous(tl.multiple_of(offs_bn, Bc), Bc)
+
+    offs_k = tl.arange(0, Bk)
+    a_ptrs = A + (offs_am[:, None] * am_stride + offs_k[None, :] * ak_stride)
+    b_ptrs = B + (offs_k[:, None] * bn_stride  + offs_bn[None, :] * bk_stride)
+
+    accumulator = tl.zeros((Br, Bc), dtype=tl.float32)
+    for k in range(0, tl.cdiv(K, Bk)):
+
+        a = tl.load(a_ptrs, mask=offs_k[None, :] < K - k * Bk, other=0.0)
+        b = tl.load(b_ptrs, mask=offs_k[:, None] < K - k * Bk, other=0.0)
+
+        accumulator = tl.dot(a, b, accumulator)
+        a_ptrs += Bk * ak_stride
+        b_ptrs += Bk * bn_stride 
+
     
-    pid = tl.program_id(0)
-    
-    num_pids_m = tl.cdiv(M, Br)
-    num_pids_n = tl.cdiv(N, Bc)
+    c = accumulator.to(tl.float16)
 
-    pids_per_group = num_pids_n * GROUP_SZE_M
-    group_id = pid//pids_per_group
-    start_pid_m = group_id*GROUP_SZE_M
-
-    group_size_m = min(GROUP_SZE_M, num_pids_m-start_pid_m)
-
-    pid_m = start_pid_m + (pid%pids_per_group)%group_size_m
-    pid_n = (pid%pids_per_group)//group_size_m
-
-    off_m = tl.arange(0, Br)
-    off_k = tl.arange(0, Bk)
-    off_n = tl.arange(0, Bc)
-
-    a_offset = off_m[:, None]*am_stride + off_k[None, :]*ak_stride
-    b_offset = off_k[:, None]*bk_stride + off_n[None, :]*bn_stride
-    
-    a_start_pos = A + pid_m*Br*am_stride
-    b_start_pos = B + pid_n*Bc*bn_stride
-    a_ptrs = (a_start_pos + a_offset)
-    b_ptrs = (b_start_pos + b_offset)
-
-    
-    acc = tl.zeros([Br, Bc], dtype = tl.float32)
-    for i in range(0, tl.cdiv(K, Bk)):
-        mask_a = (off_k[None, :]+ (i*Bk))< K 
-        mask_b = (off_k[:, None] + (i*Bk))< K 
-
-        a = tl.load(a_ptrs, mask=mask_a , other= 0.0)
-        b = tl.load(b_ptrs, mask=mask_b , other= 0.0)
-
-        acc = tl.dot(a, b, acc, allow_tf32=False)
-       
-        a_ptrs += Br*ak_stride
-        b_ptrs += Bc*bk_stride
-    
-    cm_off = pid_m * Br + tl.arange(0, Br)
-    cn_off = pid_n * Bc + tl.arange(0, Bc)
-    
-    c_offsets = cm_off[:, None]*cm_stride + cn_off[None, :]*cn_stride
-    c_ptrs = (C + c_offsets)
-    c = tl.load(c_ptrs)
-    
-    out = alpha*acc + beta*c
-    cmask =(cm_off[:, None]< M) & (cn_off[None, :] < N)
-    tl.store(c_ptrs, out, mask = cmask)
+    offs_cm = pid_m * Br + tl.arange(0, Br)
+    offs_cn = pid_n * Bc + tl.arange(0, Bc)
+    c_ptrs = C + cm_stride * offs_cm[:, None] + cn_stride * offs_cn[None, :]
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    tl.store(c_ptrs, c, mask=c_mask)
 
 
 
-
-def kernel(A, B, C, alpha, beta):
-    
-    Br = Bk = Bc = 64
-    GROUP_SIZE_M = 8
+def kernel(A, B, C):
     M, N = C.shape
     M, K = A.shape
 
-    grouping[(triton.cdiv(M,Br) * triton.cdiv(N, Bc), 1)](A, B, C, alpha, beta, 
-                                                   Br, Bc, Bk, GROUP_SIZE_M,
+    grid = lambda META: (triton.cdiv(M, META['Br']) * triton.cdiv(N, META['Bc']), 1)
+    
+    grouping[grid](A, B, C,
                                                    A.stride(0), A.stride(1),
                                                    B.stride(0), B.stride(1),
                                                    C.stride(0), C.stride(1),
-                                                   M, N, K, num_warps = 4, num_stages = 2)
-    
+                                                   M, N, K)
     return C
 
 
-def normal_impl(A, B, C, alpha, beta):
-    
-    acc = torch.matmul(A,B)
-    fp = alpha*acc
-    sp = beta*C
-    
-    return (fp + sp)
+def normal_impl(A, B):
+
+    return torch.matmul(A,B)
 
 def test():
     torch.manual_seed(20)
 
-    M = K = N = 4092
+    M = K = N = 4096
     device = 'cuda:0'
-    dtype = torch.float32
+    dtype = torch.float16
     A = torch.randn(size = (M, K), device=device, dtype=dtype)
     B = torch.randn(size = (K, N), device=device, dtype=dtype)
     C = torch.randn(size = (M, N), device=device, dtype=dtype)
     random.seed(20)
-    beta = random.random()
-    alpha = random.random()
 
-    ref_out = normal_impl(A, B, C, alpha, beta)
-
-    tr_out = kernel(A, B, C, alpha, beta)
+    ref_out = normal_impl(A, B)
+    B = B.T.contiguous()
+    tr_out = kernel(A, B, C)
+   
 
 
     print(f"correct: {torch.allclose(ref_out, tr_out, atol = 1e-2, rtol=0)}")
     print(f"dist: {torch.dist(ref_out, tr_out):.10f} | max dist : {(torch.max(torch.abs(ref_out - tr_out))):.10f}")
 
+def run():
+
+    M = K = N = 4096
+    device = 'cuda:0'
+    dtype = torch.float16
+    A = torch.randn(size = (M, K), device=device, dtype=dtype)
+    B = torch.randn(size = (K, N), device=device, dtype=dtype)
+    C = torch.randn(size = (M, N), device=device, dtype=dtype)
+    random.seed(20)
+
+    tr_out = kernel(A, B, C)
+
+
 @triton.testing.perf_report(
         triton.testing.Benchmark(
             x_names=['N'],
-            x_vals=[(32*i) for i in range(30, 51)],
+            x_vals=[(2**i) for i in range(12)],
             line_arg='provider',
             line_vals=['triton', 'torch'],
             line_names= ['Triton', 'Torch'],
@@ -133,21 +139,19 @@ def benchmark(N, provider):
 
     M = K = N 
     device = torch.device('cuda:0')
-    dtype = torch.float32
+    dtype = torch.float16
     A = torch.randn(size = (M, K), device=device, dtype=dtype)
     B = torch.randn(size = (K, N), device=device, dtype=dtype)
     C = torch.randn(size = (M, N), device=device, dtype=dtype)
-    random.seed(20)
-    beta = random.random()
-    alpha = random.random()
+    
     stream = getattr(torch, device.type).Stream()
     getattr(torch, device.type).set_stream(stream)
 
     if provider == 'torch':
-        ms = triton.testing.do_bench(lambda: normal_impl(A,B,C, alpha, beta))
+        ms = triton.testing.do_bench(lambda: normal_impl(A,B))
     
     if provider == 'triton':
-        ms = triton.testing.do_bench(lambda: kernel(A,B,C, alpha, beta))
+        ms = triton.testing.do_bench(lambda: kernel(A,B,C))
          
 
     gbps = lambda ms: ((2*N**3 + N**2)/1e9)/ (ms*1e-3)
@@ -155,6 +159,6 @@ def benchmark(N, provider):
     return gbps(ms)
 
 if __name__ == "__main__":
-    benchmark.run(show_plots=False, print_data=True)
 
-    test()
+    # benchmark.run(show_plots=True, print_data=True)
+    run()
